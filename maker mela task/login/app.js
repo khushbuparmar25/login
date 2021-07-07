48ce5a675bdde4c71c894c0e836daea540a880b3
khushbuparmar25 <66319265+khushbuparmar25@users.noreply.github.com>
2021-07-07 11:21:06 +0530

    trial commit

--- maker mela task/login/app.js
+++ maker mela task/login/app.js
@@ -38,6 +38,7 @@ app.use(session({
     saveUninitialized:true
 }))
 
+
 app.use(require('connect-flash')());
 app.use(function (req, res, next) {
   res.locals.messages = require('express-messages')(req, res);
